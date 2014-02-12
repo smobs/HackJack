6f1e40951e224700368b90c879406e6e7d5b4b4b
smobs <tobs169@gmail.com>
2014-02-12 22:03:06 +0000

    Use a custom data generator to create hand with no duplicates

--- Tests/BlackjackTests.hs
+++ Tests/BlackjackTests.hs
@@ -7,11 +7,19 @@ import Tests.CardTestInstances
 
 prop_deck_has_one_of_each_card c = length (filter (c ==) blackjackDeck) == 1
 
-prop_twelve_card_hand_must_bust hand = length hand >= 12 ==> computeHandValue hand == Bust
+prop_twelve_card_hand_must_bust = forAll (handGenerator 52) (\h -> length h >= 12 ==> computeHandValue h == Bust)
 
-prop_two_card_hand_cant_bust hand = length hand <=2 ==> computeHandValue hand /= Bust
+prop_two_card_hand_cant_bust = forAll (handGenerator 2) (\h -> computeHandValue h /= Bust)
 
 main = do quickCheck prop_deck_has_one_of_each_card
           quickCheck prop_twelve_card_hand_must_bust
           quickCheck prop_two_card_hand_cant_bust
+          
+handGenerator :: Int -> Gen Hand
+handGenerator maxsize = do i <- choose(0, maxsize)
+                           return (handGenerator' blackjackDeck i)
 
+handGenerator' :: DeckOfCards -> Int -> Hand
+handGenerator' [] _ = []
+handGenerator' _ 0 = []
+handGenerator' (c:d) i = c : (handGenerator' d (i - 1))
